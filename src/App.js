49db36e64dd8c4a351eb5d53e637eb780cded159
KlausB-ctrl <ryanantjackson@gmail.com>
2022-08-30 13:33:23 +1000

    Structure stuff, still WIP

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import BoardList from './components/BoardList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="sidebar">
+        <div className="sidebar__logo">
+          <img src={require('./images/logo.png')} alt="logo"></img>
+        </div>
+        <div className="sidebar__content">
+          <h2>ALL BOARDS</h2>
+          <BoardList />
+        </div>
+      </div>
+      <div className="content">
+        <header>
+          <div className="header__content">
+            <h1>Task Management App</h1>
+            <div className="header__buttons">
+              <button className="button--green">+Add New Task</button>
+              <div className="three-dot-button"></div>
+            </div>
+          </div>
+        </header>
+        <div className="tasks">
+          <div className="tasks__column">
+            <div className="tasks__title"></div>
+            <div className="task__container">
+              <h3>Create Figma Sketches</h3>
+              <p>1 of 1 subtasks</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/BoardList.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function BoardList(props) {
+    return(<div className="sidebar__content__boardlist">
+        <button className="boardlist__button--active">Task Management</button>
+        <button>Task Management</button>
+        <button>Task Management</button>
+        <button>Task Management</button>
+        <button>Task Management</button>
+    </div>);
+}
+
+export default BoardList;
